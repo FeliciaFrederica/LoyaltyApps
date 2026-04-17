6295500219ef46cbc60070fb254c761b13b8aad2
FeliciaFrederica <feliciafrederica1974@gmail.com>
2026-04-17 16:57:28 +0700

    update user schema (role, points, vouchers, etc)

--- src/models/users-schema.js
+++ src/models/users-schema.js
@@ -2,8 +2,37 @@ module.exports = (db) =>
   db.model(
     'Users',
     db.Schema({
-      email: String,
-      password: String,
-      fullName: String,
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      fullName: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+      },
+      points: {
+        type: Number,
+        default: 0,
+      },
+      vouchers: [
+        {
+          type: db.Schema.Types.ObjectId,
+          ref: 'Vouchers',
+        },
+      ],
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
     })
   );
