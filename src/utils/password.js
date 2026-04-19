6eafef7c88b0122edd15943d3896783eee09d291
FeliciaFrederica <feliciafrederica1974@gmail.com>
2026-04-19 20:54:35 +0700

    update auth-service, authentication, & password

--- src/utils/password.js
+++ src/utils/password.js
@@ -7,18 +7,7 @@ const bcrypt = require('bcrypt');
  */
 async function hashPassword(password) {
   const saltRounds = 16;
-
-  const hashedPassword = await new Promise((resolve, reject) => {
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(hash);
-      }
-    });
-  });
-
-  return hashedPassword;
+  return bcrypt.hash(password, saltRounds);
 }
 
 /**
@@ -29,7 +18,7 @@ async function hashPassword(password) {
  * @returns {boolean}
  */
 async function passwordMatched(password, hashedPassword) {
-  return bcrypt.compareSync(password, hashedPassword);
+  return bcrypt.compare(password, hashedPassword);
 }
 
 module.exports = {
