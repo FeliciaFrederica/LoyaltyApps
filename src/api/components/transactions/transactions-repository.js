f4c75ca80cb9af246d33830dc76810bf68249b39
vioshellina <agitaputri05@gmail.com>
2026-04-22 12:32:43 +0700

    fix conflict

--- src/api/components/transactions/transactions-repository.js
+++ src/api/components/transactions/transactions-repository.js
@@ -5,37 +5,22 @@ async function createTransaction(data) {
 }
 
 async function getTransactionHistory(userId) {
-<<<<<<< HEAD
-  return Transactions.find({userId}).sort({date: -1});
+  return Transactions.find({ userId })
+    .populate('productId')
+    .sort({ date: -1 });
 }
 
 async function getVoucherById(id) {
   return Vouchers.findById(id);
-=======
-  return Transactions.find({ userId }).populate('productId').sort({ date: -1 });
->>>>>>> main
 }
 
 async function getProductById(id) {
   return Products.findById(id);
-<<<<<<< HEAD
-=======
-}
-
-async function getUserById(id) {
-  return Users.findById(id);
->>>>>>> main
 }
 
 module.exports = {
   createTransaction,
   getTransactionHistory,
-<<<<<<< HEAD
   getVoucherById,
-  getProductById
-};
-=======
   getProductById,
-  getUserById,
 };
->>>>>>> main
