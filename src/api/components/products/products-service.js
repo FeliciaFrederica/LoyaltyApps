e65fe4510c34a6bdf911432b92f5465d8b11f08a
feli-marmar <feliciamputri@gmail.com>
2026-04-22 10:37:46 +0700

    update products price

--- src/api/components/products/products-service.js
+++ src/api/components/products/products-service.js
@@ -16,14 +16,15 @@ async function createProducts(data) {
   }
 
   // validasi apakah nominal harga yang dimasukkan bernilai minus
-  if (price < 0) {
-      const error = new Error('Harga tidak valid! Harga tidak boleh kurang dari 0.');
+  if (price <= 0) {
+      const error = new Error('Harga tidak valid! Harga harus lebih besar dari 0.');
       error.status = 400;
       throw error;
   }
 
   // cek apakah ada duplikasi nama products
-  const existingProduct = await productsRepository.findByName(name.trim());
+  const formattedName = name.trim().toLowerCase();
+  const existingProduct = await productsRepository.findByName(formattedName);
   
   if (existingProduct) {
     const error = new Error('Produk sudah terdaftar!');
@@ -31,7 +32,7 @@ async function createProducts(data) {
     throw error;
   }
 
-  const newProduct = await productsRepository.createProduct(
+  const newProducts = await productsRepository.createProducts(
     name.trim(),
     price,
     stock,
@@ -41,7 +42,7 @@ async function createProducts(data) {
   return {
     success: true,
     message: 'Produk berhasil ditambahkan',
-    data: newProduct
+    data: newProducts
   };
 
 }
